f8ee71e72cd20ff6d5043210038fd642c00f6331
Lucas Damo <lucas.damo@edu.pucrs.br>
2025-05-06 02:22:35 -0300

    working gauss jacobi

--- Makefile
+++ Makefile
@@ -1,30 +1,28 @@
-# Compiler and flags
 CXX = g++
 CXXFLAGS = -Wall -Wextra -g -std=c++20
 
-# Directories
 SRC_DIR = src
 BIN_DIR = bin
+CASE_DIR = cases
 
-# Target executable
 TARGET = $(BIN_DIR)/program
 
-# Find all source files
 SRCS = $(wildcard $(SRC_DIR)/*.cpp)
 
-# Default rule
 all: $(TARGET)
 
-# Compile and link source files to create the executable
 $(TARGET): $(SRCS)
 	mkdir -p $(BIN_DIR)
 	$(CXX) $(CXXFLAGS) -o $@ $^
-run: $(TARGET)
-	./$(TARGET)
 
-# Clean rule to remove generated files
+run: $(TARGET)
+	./$(TARGET) $(CASE_DIR)/caso10.txt 300000; \
+	./$(TARGET) $(CASE_DIR)/caso20.txt 300000; \
+	./$(TARGET) $(CASE_DIR)/caso30.txt 300000; \
+	./$(TARGET) $(CASE_DIR)/caso50.txt 300000; \
+	./$(TARGET) $(CASE_DIR)/caso100.txt 300000; \
+	./$(TARGET) $(CASE_DIR)/caso150.txt 300000; \
+	./$(TARGET) $(CASE_DIR)/caso200.txt 300000;
+	
 clean:
 	rm -rf $(BIN_DIR)
-
-# Phony targets
-.PHONY: all clean
--- src/sys-solver.cpp
+++ src/sys-solver.cpp
@@ -5,19 +5,43 @@
 #include <unordered_map>
 #include <string>
 #include <limits>
+#include <iomanip> 
 
 using namespace std;
 
-int main() {
-    ifstream file("cases/caso10.txt");
+int check_convergence(unordered_map<string, double> previous, unordered_map<string, double> current, double tolerance){
+    double max_diff = 0;
+
+    for(const auto& pair : previous){
+        string key = pair.first;
+        double prev_value = pair.second;
+        double curr_value = current[key];
+        double diff = abs(curr_value - prev_value);
+        if(diff > max_diff){
+            max_diff = diff;
+        }
+    }
+    return max_diff <= tolerance ? 0 : 1;
+}
+
+int main(int argc, char* argv[]) {
+    string filename = argv[1];
+    ifstream file(filename);
+    if (!file.is_open()) {
+        cerr << "Error: Could not open file " << filename << endl;
+        return 1;
+    } else {
+        cout << "==============================" << endl;
+        cout << "solving :" << filename << endl;
+    }
     string line;
 
-    unordered_map<string, int> cityToIndex;
-    unordered_map<string, int> cityToAmount;
+    unordered_map<string, double> cityToIndex;
+    unordered_map<string, double> cityToAmount;
+    unordered_map<string, double> cityToAmount_previous;
     vector<string> indexToCity;
-    vector<tuple<string, string, int>> connections;
+    vector<tuple<string, string, double>> connections;
 
-    // First pass: collect cities and values
     while (getline(file, line)) {
         stringstream ss(line);
         string city1, city2;
@@ -26,7 +50,6 @@ int main() {
         ss >> city1;
         if (ss >> city2) {
             if (ss >> value) {
-                // This line has a connection (3 entries)
                 connections.emplace_back(city1, city2, value);
                 if (cityToIndex.find(city1) == cityToIndex.end()) {
                     cityToIndex[city1] = cityToIndex.size();
@@ -37,27 +60,58 @@ int main() {
                     indexToCity.push_back(city2);
                 }
             } else {
-                // This line has a city and its value (2 entries)
-                if (cityToIndex.find(city1) == cityToIndex.end()) {
-                    cityToIndex[city1] = cityToIndex.size();
                     cityToAmount[city1] = stoi(city2);
-                    indexToCity.push_back(city1);
-                }
+                    connections.emplace_back(city1, city1, stoi(city2));
             }
         }
     }
 
     int N = cityToIndex.size();
-    vector<vector<int>> matrix(N, vector<int>(N, 0)); // or INT_MAX if you prefer
+    vector<vector<double>> matrix(N, vector<double>(N, 0)); 
 
-    // Populate the matrix with the connections
     for (const auto& [from, to, value] : connections) {
-        int i = cityToIndex[from];
-        int j = cityToIndex[to];
+        int i = cityToIndex[to];
+        int j = cityToIndex[from];
         matrix[i][j] = value;
     }
 
 
+    for(int max_it = 0; max_it < stoi(argv[2]); max_it++){
+        cityToAmount_previous = cityToAmount;
+        for(int i = 0; i < N; i++){
+            double accumm = 0;
+            for(int j = 0; j < N; j++){
+                if(i == j){
+                    accumm += matrix[i][j];
+                } else {
+                    accumm += cityToAmount[indexToCity[j]] * (double(matrix[i][j])/100);
+                }
+            }
+            cityToAmount[indexToCity[i]] = accumm;
+        }
+        if(!check_convergence(cityToAmount_previous, cityToAmount, 1e-6)){
+            break;
+        }
+    }
+
+    double max_people = numeric_limits<double>::lowest();
+    double min_people = numeric_limits<double>::max();
+    string max_city, min_city;
+
+    for (const auto& pair : cityToAmount) {
+        if (pair.second > max_people) {
+            max_people = pair.second;
+            max_city = pair.first;
+        }
+        if (pair.second < min_people) {
+            min_people = pair.second;
+            min_city = pair.first;
+        }
+    }
+
+    cout << fixed << setprecision(5);
+    cout << "City with the most amount of arrivals : " << max_city << " -> " << max_people << endl;
+    cout << "City with the least amount of arrivals: " << min_city << " -> " << min_people << endl;
 
     return 0;
 }
